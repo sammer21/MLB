4a3056bb8db1d3d10c06e86744aabeea0b991da8
Sam Erickson <sameslc21@gmail.com>
2025-01-06 16:15:25 +0000

    dim_contracts docs and final model

--- models/dimension/players/model/dim_players.sql
+++ models/dimension/players/model/dim_players.sql
@@ -1,71 +1,18 @@
 {{ config(materialized='table') }}
 
+{% set teams = [
+    'guardians', 'royals', 'tigers', 'twins', 'white_sox', 'blue_jays', 'orioles', 'rays', 'redsox', 'yankees', 
+    'angels', 'athletics', 'astros', 'rangers', 'mariners', 'brewers', 'cardinals', 'cubs', 'pirates', 'reds',     
+    'braves', 'marlins', 'mets', 'nationals', 'phillies', 'dodgers', 'diamondbacks', 'giants', 'padres', 'rockies' 
+] %}
+
 with players as (
-    -- AL Central
-    select * from {{ ref('stg_guardians_roster') }}
-    union all
-    select * from {{ ref('stg_royals_roster') }}
-    union all
-    select * from {{ ref('stg_tigers_roster') }}
-    union all
-    select * from {{ ref('stg_twins_roster') }}
-    union all
-    select * from {{ ref('stg_white_sox_roster') }}
-    -- AL East 
-    union all
-    select * from {{ ref('stg_blue_jays_roster') }}
-    union all
-    select * from {{ ref('stg_orioles_roster') }}
-    union all
-    select * from {{ ref('stg_rays_roster') }}
-    union all
-    select * from {{ ref('stg_redsox_roster') }}
-    union all
-    select * from {{ ref('stg_yankees_roster') }}
-    -- AL West
-    union all
-    select * from {{ ref('stg_angels_roster') }}
-    union all
-    select * from {{ ref('stg_athletics_roster') }}
-    union all
-    select * from {{ ref('stg_astros_roster') }}
-    union all
-    select * from {{ ref('stg_rangers_roster') }}
-    union all
-    select * from {{ ref('stg_mariners_roster') }}
-    -- NL Central 
-    union all
-    select * from {{ ref('stg_brewers_roster') }}
-    union all
-    select * from {{ ref('stg_cardinals_roster') }}
-    union all
-    select * from {{ ref('stg_cubs_roster') }}
-    union all
-    select * from {{ ref('stg_pirates_roster') }}
-    union all
-    select * from {{ ref('stg_reds_roster') }}
-    -- NL East
-    union all
-    select * from {{ ref('stg_braves_roster') }}
-    union all
-    select * from {{ ref('stg_marlins_roster') }}
-    union all
-    select * from {{ ref('stg_mets_roster') }}
-    union all
-    select * from {{ ref('stg_nationals_roster') }}
-    union all
-    select * from {{ ref('stg_phillies_roster') }}
-    -- NL West
-    union all
-    select * from {{ ref('stg_dodgers_roster') }}
-    union all
-    select * from {{ ref('stg_diamondbacks_roster') }}
-    union all
-    select * from {{ ref('stg_giants_roster') }}
-    union all
-    select * from {{ ref('stg_padres_roster') }}
-    union all
-    select * from {{ ref('stg_rockies_roster') }}
+    {% for team in teams %}
+        {% if not loop.first %}
+            union all
+        {% endif %}
+        select * from {{ ref('stg_' ~ team ~ '_roster') }}
+    {% endfor %}
 )
 
 select {{ dbt_utils.generate_surrogate_key(['player_team_name', 'player_name', 'player_age'] ) }}as player_key,
