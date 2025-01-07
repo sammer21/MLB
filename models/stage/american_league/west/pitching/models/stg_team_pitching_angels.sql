3a7c4cd87107990d03840208991f9edf05979e8a
Sam Erickson <sameslc21@gmail.com>
2025-01-07 17:37:43 +0000

    optimize pitching

--- models/stage/american_league/west/pitching/models/stg_team_pitching_angels.sql
+++ models/stage/american_league/west/pitching/models/stg_team_pitching_angels.sql
@@ -1,10 +1,6 @@
 {% set columns = ['W', 'L', 'W-Lpct', 'ERA', 'G', 'GS', 'GF', 'CG', 'SHO', 'SV', 'IP', 'H', 'R', 'ER', 'HR', 
 'BB', 'IBB', 'SO', 'HBP', 'BK', 'WP', 'BF', 'ERAplus', 'FIP', 'WHIP', 'H9', 'HR9', 'BB9', 'SO9', 'SO-W']%}
 
-with pitching as (
-    select * from {{ ref('team_pitching_angels') }}
-)
-
 select 'Angels' as player_team_name 
     , Rk as player_rank
     , Name as player_name
@@ -14,4 +10,4 @@ select 'Angels' as player_team_name
             coalesce(`{{ col }}`, 0) as `{{ col | lower}}`{% if not loop.last %}, {% endif %}          -- adds a coalesce function through all remaining metrics
         {% endfor %}
 
-from pitching
+from {{ ref('team_pitching_angels') }}
